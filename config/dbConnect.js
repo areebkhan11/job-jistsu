a9d0db6be43f393569c0b6c9c232cfa862b3a22f
Makhmoor <makhmoor.rehman0@gmail.com>
2024-12-23 17:29:10 +0500

    refactor

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGODB_URL)
-        .then(() => console.log('Connected to DB'))
-        .catch((error) => console.log("db error: ", error));
-}
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    })
+    .then(() => console.log("Connected to DB"))
+    .catch((error) => console.log("db error: ", error));
+};
